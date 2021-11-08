95a35cce108ad31629845edc9967f56a32524d34
kongukjae <kongukjae@gmail.com>
2021-11-08 15:43:57 +0900

    [20211108] 강의 정보

--- slide/fadein-alrogithm.js
+++ slide/fadein-alrogithm.js
@@ -5,14 +5,18 @@ function clickHandle(eventClass) {
 }
 
 function fade(element) {
+//  주요알고리즘
   let opacityValue = 1; // initial setting
   const timer = setInterval(function () {
     if (opacityValue <= 0.1) {
       clearInterval(timer);
+      // 구체적인 값 핸들링
       element.style.display = 'none';
     }
+      // 구체적인 값 핸들링
     element.style.opacity = opacityValue;
     element.style.filter = `alpha(opacity= ${opacityValue} * 100)`;
+    // 주요알고리즘
     opacityValue -= opacityValue * 0.1;
   }, 50);
 }

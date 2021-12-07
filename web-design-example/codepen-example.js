52a198a77c4085866c6eb2c2d83062c2ebeb7c39
kongukjae <kongukjae@gmail.com>
2021-12-07 11:11:31 +0900

    functional example

--- web-design-example/codepen-example.js
+++ web-design-example/codepen-example.js
@@ -1,10 +1,14 @@
 var hits = 0;
 var hitElement = document.querySelector( '.hits' );
+
+// **************************************
 document.body.onkeyup = function(e) {
   if( e.keyCode == 32 ) {
     addHit();
   }
 }
+
+
 
 var addHit = function() {
   hits++;
@@ -19,3 +23,40 @@ var resetHits = function() {
   hits = 0;
   renderHits();
 }
+
+
+
+
+
+// 이런방식은 구현 만시켜놓은 것 -> 범용적이진 않다.
+
+const a = 1;
+const b = 2;
+const c = document;
+// ****************************************************
+function sum(selector, targetA, targetB) {
+  selector.addEventListener('click', function() {
+    return targetA + targetB;
+  });
+}
+sum(d, a, 4);
+// *****************************************************
+c.addEventListener('click', function() {
+  a + b;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Element.addEventListener()
